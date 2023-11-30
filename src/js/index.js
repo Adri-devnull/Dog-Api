86ace2453425e70fe88befa6bed0655b98c34ca4
Adri-devnull <adri.devnull@gmail.com>
2023-11-30 19:59:07 +0100

    second-changes

--- src/js/index.js
+++ src/js/index.js
@@ -14,8 +14,9 @@
 const breedListElement = document.getElementById('breedList');
 const subBreedListElement = document.getElementById('subBreedList');
 const randomDogBtnElement = document.getElementById('button');
-
-
+const labelSubBreedElement = document.getElementById('label-subbreed');
+const buttonSaveElement = document.getElementById('button-save');
+const containerLsImagesElement = document.getElementById('container-local-storage');
 
 // FUNCION PARA PINTAR EL ERROR
 const printError = (err) => console.log(err);
@@ -41,42 +42,74 @@ const fillInOptions = async () => {
     }
 };
 
+// LLAMADA A LA FUNCION PARA MOSTRAR SELECT CON LAS RAZAS DISPONIBLES
 fillInOptions();
 
 // FUNCION PARA GUARDAR LA IMAGEN EN EL LS
 const saveImageLs = (number, imageUrls) => {
-    const savedImages = JSON.parse(localStorage.getItem('savedImages'));
+    let savedImages = JSON.parse(localStorage.getItem('savedImages'));
+
+    if (!Array.isArray(savedImages)) {
+        savedImages = []; // Si no hay datos, crea un nuevo array vacío
+    }
+
     savedImages.push(imageUrls[number]);
     localStorage.setItem('savedImages', JSON.stringify(savedImages));
-    alert('Imagen guardada en el Local Storage');
 };
 
-// FUNCION PARA RECIBIR IMAGENES DE LA API
-const getImagesDog = async (breed) => {
+
+// FUNCION PARA RECIBIR IMAGENES DE LA API Y PINTAR DEPENDIENDO DE LA RAZA O SUBRAZA SELECCIONADA
+const getImagesDog = async (breed, subbreed) => {
     try {
-        const data = await fetchData(`https://dog.ceo/api/breed/${breed}/images`);
-        const imageUrls = data.message;
+        if (subbreed === '') {
+            const data = await fetchData(`https://dog.ceo/api/breed/${breed}/images`);
+            const imageUrls = data.message;
 
-        const randomNumber = Math.floor(Math.random() * imageUrls.length);
-        const imageDogElement = document.getElementById('image');
-        imageDogElement.src = imageUrls[randomNumber];
+            const randomNumber = Math.floor(Math.random() * imageUrls.length);
+            const imageDogElement = document.getElementById('image');
+            imageDogElement.src = imageUrls[randomNumber];
 
-        const saveButton = document.getElementById('button-save');
-        saveButton.addEventListener('click', () => {
-            saveImageLs(randomNumber, imageUrls);
-        });
+            const saveButton = document.getElementById('button-save');
+            saveButton.addEventListener('click', () => {
+                saveImageLs(randomNumber, imageUrls);
+            });
+        } else {
+            const data = await fetchData(`https://dog.ceo/api/breed/${breed}/${subbreed}/images`);
+            const imageUrls = data.message;
+            const randomNumber = Math.floor(Math.random() * imageUrls.length);
+            const imageDogElement = document.getElementById('image');
+            imageDogElement.src = imageUrls[randomNumber];
+            const saveButton = document.getElementById('button-save');
+            saveButton.addEventListener('click', () => {
+                saveImageLs(randomNumber, imageUrls);
+            });
+        }
     } catch (error) {
         printError(error);
     }
+    buttonSaveElement.classList.remove('hide');
+    buttonSaveElement.classList.add('show');
 };
 
 // FUNCION PARA CUANDO CARGUE LA PAGINA PINTAR LAS IMAGENES QUE ESTEN GUARDADAS EN EL LS
+const getImagesLs = () => {
+    const lsImages = JSON.parse(localStorage.getItem('savedImages'));
+    lsImages.forEach(image => {
+        const img = document.createElement('img');
+        const button = document.createElement('button');
+        button.textContent = 'X';
+        img.src = image;
+        img.append(button)
+        containerLsImagesElement.append(img);
+    })
+}
+// LLAMADA A LA FUNCION PARA MOSTRAR IMAGENES FAVORITAS GUARDADAS EN EL LS
+getImagesLs();
 
 
 // FUNCION PARA PINTAR LAS OPCIONES CON LAS RAZAS DE LA API
 const fillInBreeds = (data) => {
     const breeds = Object.keys(data.message);
-
     for (let i = 0; i < breeds.length; i++) {
         const breed = breeds[i];
         const subBreeds = data.message[breed];
@@ -110,9 +143,13 @@ const fillInBreeds = (data) => {
                 nameOfSubBreed.textContent = subBreeds[j];
                 subBreedListElement.append(nameOfSubBreed);
             }
+            labelSubBreedElement.classList.remove('hide');
+            labelSubBreedElement.classList.add('show');
             subBreedListElement.classList.remove('hide');
             subBreedListElement.classList.add('show');
         } else {
+            labelSubBreedElement.classList.remove('show');
+            labelSubBreedElement.classList.add('hide');
             subBreedListElement.classList.remove('show');
             subBreedListElement.classList.add('hide');
         }
@@ -120,7 +157,9 @@ const fillInBreeds = (data) => {
 
 };
 
+// LLAMAMOS AL EVENTO DE ESCUCHA PARA PINTAR LA IMAGEN
 randomDogBtnElement.addEventListener('click', () => {
     const selectedBreed = breedListElement.value;
-    getImagesDog(selectedBreed);
+    const selectedSubBreed = subBreedListElement.value;
+    getImagesDog(selectedBreed, selectedSubBreed);
 });
